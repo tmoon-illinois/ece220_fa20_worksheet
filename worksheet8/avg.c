1642dd6e999928e0ac90817058eb008895576b36
Thomas Moon <tmoon@linux-a2.ews.illinois.edu>
2020-09-17 09:27:17 -0500

    ws 8

---
+++ worksheet8/avg.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n, i, num, sum=0;
+    double average;
+    printf("Enter how many numbers to input: ");
+    scanf("%d", &n);
+
+    /* Your code here */
+
+
+    printf("The average of the numbers: %lf\n", average);
+}
---
+++ worksheet8/calendar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int date;
+    printf("Enter a date in September 2020: ");
+    scanf("%d", &date);
+    /* Your code here */
+    
+    return 0;
+}
